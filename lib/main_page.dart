f6e74d19e14cd22df6a9ad3de7bc919d7d495888
Sahil Barua <sahilrik@gmail.com>
2022-02-23 00:54:01 +0530

    sliver app bar completed error in floating

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -6,17 +6,32 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('sliver app bar'),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            floating: true,
+            snap: false,
+            pinned: true,
+            expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text('sliver app bar'),
+              background: Image.network(
+                'https://images.unsplash.com/photo-1641115948629-6ff88111fbeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+        body: ListView.separated(
+            padding: const EdgeInsets.all(8.0),
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('item $index'),
+              );
+            },
+            separatorBuilder: (context, int index) => const Divider(),
+            itemCount: 30),
       ),
-      body: ListView.separated(
-          itemBuilder: ((context, index) {
-            return ListTile(
-              title: Text('item $index'),
-            );
-          }),
-          separatorBuilder: (context, index) => const Divider(),
-          itemCount: 25),
     );
   }
 }
